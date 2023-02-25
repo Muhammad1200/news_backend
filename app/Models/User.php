2839cfeca935b4d1854459abbc9a61aace9452dd
Muhammad1200 <usamaasif4190@gmail.com>
2023-02-25 14:29:02 +0500

    Add Relation in User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
         $query->with($this->relation);
     }
 
+    public function interests()
+    {
+        return $this->hasOne(Interest::class,'user_id','id');
+    }
+
 }
